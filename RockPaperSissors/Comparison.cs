a661ff10992ac9cb6f6ef7e1b5e30bd65e9e7095
Kiyanzalegend <chesley.walker@gmail.com>
2017-09-20 18:51:57 -0500

    init commit

---
+++ RockPaperSissors/Comparison.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperSissors
+{
+    class Comparison
+    {
+        public static void Rock(int cpuChoice)
+        {
+            if (cpuChoice == 0)
+            {
+                Console.WriteLine("We tied try again!");
+            }
+            if (cpuChoice == 1)
+            {
+                Console.WriteLine("I chose scissors, you win!");
+            }
+            if (cpuChoice == 2)
+            {
+                Console.WriteLine("I chose paper, you lose!");
+            }
+        }
+        public static void Scissors(int cpuChoice)
+        {
+            if (cpuChoice == 0)
+            {
+                Console.WriteLine("I chose rock, you lose!");
+            }
+            if (cpuChoice == 1)
+            {
+                Console.WriteLine("We tied, try again!");
+            }
+            if (cpuChoice == 2)
+            {
+                Console.WriteLine("I chose paper, you win!");
+            }
+        }
+        public static void Paper (int cpuChoice)
+        {
+            if (cpuChoice == 0)
+            {
+                Console.WriteLine("I chose rock, you win!");
+            }
+            if (cpuChoice == 1)
+            {
+                Console.WriteLine("I chose scissors, you lose!");
+            }
+            if (cpuChoice == 2)
+            {
+                Console.WriteLine("We tied, try again!");
+            }
+        }
+        public static void PlayAgain()
+        {
+            
+        }
+    }
+}
--- RockPaperSissors/Program.cs
+++ RockPaperSissors/Program.cs
@@ -4,12 +4,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RockPaperSissors
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool continueGame = true; 
+            Random rng = new Random();
+            
+            do
+            {
+                Console.WriteLine("Please Enter Rock, Paper, or Sissors");
+            var input = Console.ReadLine();
+            int cpuChoice = rng.Next(2);
+                input.ToLower();
+
+                if(input == "rock")
+                {
+                    Comparison.Rock(cpuChoice);
+                }
+                if (input == "scissors")
+                {
+                    Comparison.Scissors(cpuChoice);
+                }
+                if (input == "paper")
+                {
+                    Comparison.Paper(cpuChoice);
+                }
+                Console.WriteLine("Play again? y/n");
+                string nextGame = Console.ReadLine();
+                char[] testArray = new char[1];
+                foreach (char c in nextGame)
+                {
+                    int i = 0;
+                    testArray.SetValue(c, i);
+                    i++;
+                }
+                if (testArray[0].ToString() != "y" || testArray[0].ToString() != "n" )
+                {
+                    Console.WriteLine("Play again? y/n");
+                    
+                    foreach (char c in nextGame)
+                    {
+                        int i = 0;
+                        testArray.SetValue(c, i);
+                        i++;
+                    }
+                }
+                //Console.WriteLine(nextGame.ToString());
+                switch (testArray[0].ToString().ToLower())
+                {
+                    case "y":
+                        continueGame = true;
+                        break;
+                    case "n":
+                        continueGame = false;
+                        break;
+                }
+                nextGame = string.Empty;
+                input = string.Empty;
+                 
+            }
+            while (continueGame);
+            Console.WriteLine("Thanks for playing! \nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
